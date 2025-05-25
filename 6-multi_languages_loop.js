d553fed90390bf9e79b7d0d71ca2bbd39b467782
Henry Masereti <maseretihenry5@gmail.com>
2025-05-25 17:40:19 +0300

    Add multi-language loop script for ALX

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,11 @@
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-let output = lines.join('\n');
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+
+for (let i = 0; i < messages.length; i++) {
+  output += messages[i];
+  if (i !== messages.length - 1) output += "\n";
+}
+
 console.log(output);
+
 
